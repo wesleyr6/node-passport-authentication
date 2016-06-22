69b968ebbd47546b58531af09ed50fd7ba4dd701
Wesley Amaro <maz_wamaro@uolinc.com>
2016-06-21 21:12:46 -0300

    Add new params to collection

--- models/user.js
+++ models/user.js
@@ -4,9 +4,22 @@ var Schema = mongoose.Schema;
 
 // set up a mongoose model and pass it using module.exports
 var User = new Schema({
-	name: String,
-	password: String,
-	admin: Boolean
+	name: {
+		type: String,
+		required: true,
+		index: {
+			unique: true
+		}
+	},
+
+	password: {
+		type: String,
+		required: true
+	},
+
+	securityPhrase: {
+		type: String
+	}
 });
 
 module.exports = mongoose.model('User', User);
