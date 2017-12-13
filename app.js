79b63a1033173d62c93b92d11db8d7b1c1f649ee
Wesley Biagi Amaro <wesley.amaro@NBQSP-FC397.fcamara.com.br>
2017-12-13 11:34:47 -0200

    taking the instance off for mongoose require

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const expressSession = require('express-session');
-const routes = require('./routes/');
-const logger = require('morgan');
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const expressSession = require("express-session");
+const routes = require("./routes/");
+const logger = require("morgan");
 
-require('./services/passport')(app);
-require('./services/handlebars')(app);
-require('./services/mongoose')(app);
+require("./services/passport")(app);
+require("./services/handlebars")(app);
+require("./services/mongoose");
 
 // Configs
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(cookieParser());
-app.use(expressSession({
-	secret: process.env.SESSION_SECRET || 'safadao',
-	resave: false,
-	saveUninitialized: false
-}));
+app.use(
+	expressSession({
+		secret: process.env.SESSION_SECRET || "safadao",
+		resave: false,
+		saveUninitialized: false
+	})
+);
 
 // Views
-app.use('/views', express.static(__dirname + '/views'));
-app.use('/static', express.static(__dirname + '/static'));
+app.use("/views", express.static(__dirname + "/views"));
+app.use("/static", express.static(__dirname + "/static"));
 
 // To parse request params in req.body json format
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+);
 
 // Routes
-app.use('/', routes);
+app.use("/", routes);
 
 module.exports = app;
