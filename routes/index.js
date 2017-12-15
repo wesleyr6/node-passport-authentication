c8e62682db07e9f1096397b6e1010365c8fe4613
Wesley Amaro <contato@wesleyamaro.com>
2017-12-15 18:54:24 -0200

    bugfix route auth

--- routes/index.js
+++ routes/index.js
@@ -1,91 +1,79 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-const passportLocal = require('passport-local');
-const User = require('../models/user');
-const bcrypt = require('bcrypt');
+const passport = require("passport");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 /* Authentications config */
 function ensureAuthentication(req, res, next) {
-	if (req.isAuthenticated()) {
+	var isAuth = req.session.passport ? true : false;
+
+	if (isAuth) {
 		next();
 	} else {
-		res.redirect('/');
+		res.redirect("/");
 	}
 }
 
-passport.use(new passportLocal.Strategy(function(username, password, done) {
-	User.findOne({
-		name: username
-	}, function(err, user) {
-		if (err) {
-			throw err;
-		}
-
-		if (!user) {
-			throw 'Authentication failed. User not found.';
-		} else if (user) {
-			bcrypt.compare(password, user.password, function(err, res) {
-				if (res) {
-					done(null, {
-						name: username
-					});
-				} else {
-					throw 'Authentication failed. Wrong password.';
-				}
-			});
-		}
-	});
-}));
-
 /* Routes */
-router.get('/', function(req, res) {
-	var isAuth = req.isAuthenticated();
+router.get("/", function(req, res) {
+	var isAuth = req.session.passport ? true : false;
+	console.log(req.session.passport);
 
 	if (isAuth) {
-		res.render('logged/index', {
-			title: 'Home - MyApp',
-			layout: 'logged/main.hbs',
-			bodyClass: 'logged',
-			isAuthenticated: req.isAuthenticated(),
-			user: req.user
+		res.render("logged/index", {
+			title: "Home - MyApp",
+			layout: "logged/main.hbs",
+			bodyClass: "logged"
 		});
 	} else {
-		res.render('unlogged/account/login', {
-			title: 'Login - MyApp',
-			layout: 'unlogged/main.hbs',
-			bodyClass: 'unlogged'
+		res.render("unlogged/account/login", {
+			title: "Login - MyApp",
+			layout: "unlogged/main.hbs",
+			bodyClass: "unlogged"
 		});
 	}
 });
 
-router.post('/', passport.authenticate('local', {
-	successRedirect: '/',
-	failureRedirect: '/'
-}));
+router.post(
+	"/",
+	passport.authenticate("local", {
+		successRedirect: "/",
+		failureRedirect: "/"
+	})
+);
 
-router.get('/logout', function(req, res) {
-	req.logout();
-	res.redirect('/');
-});
-
-router.get('/signup', function(req, res) {
-	res.render('unlogged/account/signup', {
-		title: 'Sign Up - MyApp'
+router.get("/logout", function(req, res) {
+	req.session.destroy(function(err) {
+		res.redirect("/");
 	});
 });
 
-router.post('/signup', function(req, res) {
+router.get("/signup", function(req, res) {
+	res.render("unlogged/account/signup", {
+		title: "Sign Up - MyApp"
+	});
+});
+
+router.post("/signup", function(req, res) {
 	var signupUser;
 
 	if (req.body.password !== req.body.passwordConfirm) {
-		throw 'Incorrect password confirmation';
+		throw "Incorrect password confirmation";
 	}
+
+	User.find({ email: req.body.email }, function(err, docs) {
+		if (docs.length) {
+			throw "The email " + req.body.email + " is already exist";
+			return false;
+		}
+	});
 
 	bcrypt.genSalt(10, function(err, salt) {
 		bcrypt.hash(req.body.password, salt, function(err, hash) {
 			signupUser = new User({
-				name: req.body.username,
+				name: req.body.name,
+				email: req.body.email,
 				password: hash,
 				securityPhrase: req.body.securityPhrase
 			});
@@ -95,16 +83,16 @@ router.post('/signup', function(req, res) {
 					throw err;
 				}
 
-				console.log('SignUp: User saved successfully');
+				console.log("SignUp: User saved successfully");
 
-				res.redirect('/');
+				res.redirect("/");
 			});
 		});
 	});
 });
 
-router.get('/test', ensureAuthentication, function(req, res) {
-	res.send('Test Page');
+router.get("/test", ensureAuthentication, function(req, res) {
+	res.send("Test Page");
 });
 
 module.exports = router;
--- services/passport.js
+++ services/passport.js
@@ -1,14 +1,57 @@
-const passport = require('passport');
+const passport = require("passport");
+const passportLocal = require("passport-local");
+const expressSession = require("express-session");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const keys = require("../config/keys");
 
-passport.serializeUser(function(username, done) {
-	done(null, username);
+passport.serializeUser(function(id, done) {
+	console.log("serialize", id);
+	done(null, id);
 });
 
-passport.deserializeUser(function(username, done) {
-	done(null, username);
+passport.deserializeUser(function(id, done) {
+	console.log("deserialize", id);
+	User.findById(id, function(err, user) {
+		done(err, user);
+	});
 });
 
-module.exports = (app) => {
-    app.use(passport.initialize());
-    app.use(passport.session());
-}
+passport.use(
+	new passportLocal.Strategy(function(email, password, done) {
+		User.findOne(
+			{
+				email: email
+			},
+			function(err, user) {
+				if (err) throw err;
+
+				if (!user) {
+					throw "Authentication failed. User not found.";
+				} else if (user) {
+					bcrypt.compare(password, user.password, function(err, res) {
+						if (res) {
+							done(null, {
+								id: user._id
+							});
+						} else {
+							throw "Authentication failed. Wrong password.";
+						}
+					});
+				}
+			}
+		);
+	})
+);
+
+module.exports = app => {
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.use(
+		expressSession({
+			secret: process.env.SESSION_SECRET || keys.cookieKey,
+			resave: false,
+			saveUninitialized: false
+		})
+	);
+};
