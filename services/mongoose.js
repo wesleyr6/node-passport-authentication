9a1f4beba0b5c8dff078ed1de44a1df9aad6ea61
Wesley Amaro <contato@wesleyamaro.com>
2017-12-15 18:57:08 -0200

    warnfix

--- services/mongoose.js
+++ services/mongoose.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose");
 const config = require("../config/keys");
 
-mongoose.createConnection(config.database, {
-	useMongoClient: true
-});
+mongoose.Promise = global.Promise;
+
+mongoose.connect(
+	config.localdatabase,
+	{
+		useMongoClient: true
+	},
+	err => {
+		if (err) throw err;
+		console.log("MongoDB is connected");
+	}
+);
