a2443617c2caa92ecaadfb7d4d7415d5f468ddf2
Wesley Amaro <contato@wesleyamaro.com>
2017-12-15 18:55:04 -0200

    new user model

--- models/user.js
+++ models/user.js
@@ -1,26 +1,22 @@
 // get an instance of mongoose and mongoose.Schema
 const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
+//mongoose.Promise = global.Promise;
 const Schema = mongoose.Schema;
 
 // set up a mongoose model and pass it using module.exports
 const User = new Schema({
-	name: {
+	name: String,
+	email: {
 		type: String,
-		required: true,
 		index: {
 			unique: true
 		}
 	},
-
-	password: {
-		type: String,
-		required: true
-	},
-
-	securityPhrase: {
-		type: String
-	}
+	password: String,
+	securityPhrase: String,
+	facebookID: String,
+	googleID: String,
+	token: String
 });
 
 module.exports = mongoose.model("User", User);
